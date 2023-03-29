61f35bbb2c1cfb3ecd6da903ee4d922a254aabe9
Hamed Davoodi <109058050+hameddavodi@users.noreply.github.com>
2023-03-29 12:59:50 +0200

    Update Matrix, Solutions, Eigens.py

--- Math/Matrix, Solutions, Eigens.py
+++ Math/Matrix, Solutions, Eigens.py
@@ -8,3 +8,48 @@ By finding the eigens of a matrix, we can identify the most important features o
 '''
 
 
+'''
+To create a matrix using NumPy, you can use the numpy.array() function. Here's an example:
+''' 
+
+import numpy as np
+
+# Create a 2x3 matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+
+# You can see that the format of the syntax is: np.(attribut) then followed by ( [  [],[], ..., []   ]  ). 
+# So there is a big bracket that contains small brackets indicating raws of a matrix.
+
+print(matrix)
+#Here is the output:
+###################################################
+
+[[1 2 3]
+ [4 5 6]]
+
+###################################################
+
+#You can also create matrices of zeros or ones using the numpy.zeros() and numpy.ones() functions:
+
+import numpy as np
+
+# Create a 3x3 matrix of zeros
+zeros_matrix = np.zeros((3, 3))
+
+print(zeros_matrix)
+
+###################################################
+# Output:
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+
+# Create a 2x4 matrix of ones
+ones_matrix = np.ones((2, 4))
+
+print(ones_matrix)
+###################################################
+# Output:
+[[1. 1. 1. 1.]
+ [1. 1. 1. 1.]]
+
