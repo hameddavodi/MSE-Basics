4c8d8e181acd68f489a07e2095c2bf986cb9b71e
Hamed Davoodi <109058050+hameddavodi@users.noreply.github.com>
2023-03-30 17:59:45 +0200

    Update Matrix, Solutions, Eigens.py

--- Math/Matrix, Solutions, Eigens.py
+++ Math/Matrix, Solutions, Eigens.py
@@ -1,13 +1,3 @@
-'''
-Matrix, solutions, and eigens are all essential concepts in linear algebra, which is a crucial field in data science.
-Matrices are rectangular arrays of numbers that can be used to represent data sets, with each element in the matrix representing a single data point.
-Solutions in linear algebra refer to finding the values of the variables in a system of linear equations that satisfy all the equations simultaneously.
-This is important in data science, where linear models are often used to represent and analyze data.
-Eigens are a way of describing the characteristics of a matrix, and they are often used in dimensionality reduction techniques such as principal component analysis.
-By finding the eigens of a matrix, we can identify the most important features of the data and reduce its complexity, making it easier to work with and analyze
-
-'''
-
 # To create a matrix using NumPy, you can use the numpy.array() function. Here's an example: 
 # If you got error of numpy try to install/reinstall it with: " ipython pip3 install numpy " / or just " pip3 install numpy"
 import numpy as np
