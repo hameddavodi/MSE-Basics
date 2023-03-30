fa10e4a3a0301a87c962f35643999f98f54bc02a
Hamed Davoodi <109058050+hameddavodi@users.noreply.github.com>
2023-03-30 19:49:45 +0200

    Update Confidence interval and Hypothesis testing.py

--- Stat/Confidence interval and Hypothesis testing.py
+++ Stat/Confidence interval and Hypothesis testing.py
@@ -1,1 +1,122 @@
+# To calculate the confidence interval for the mean using NumPy, you can use the numpy and scipy modules. 
+# Here's an example:
+
+import numpy as np
+from scipy import stats
+
+# generate some random data
+data = np.random.normal(loc=5, scale=2, size=100)
+
+# calculate the sample mean and standard deviation
+mean = np.mean(data)
+std = np.std(data, ddof=1)
+
+# set the confidence level and degrees of freedom
+conf_level = 0.95
+df = len(data) - 1
+
+# calculate the critical z-value
+z_crit = stats.norm.ppf((1 + conf_level) / 2, df)
+
+# calculate the margin of error
+moe = z_crit * std / np.sqrt(len(data))
+
+# calculate the confidence interval
+ci_low = mean - moe
+ci_high = mean + moe
+
+print("Data:", data)
+print("Sample mean:", mean)
+print("Sample standard deviation:", std)
+print("95% confidence interval for the mean:", (ci_low, ci_high))
+
+# Output
+Data: 
+[ 4.27219745  2.50520023  1.87609778  4.24258018  6.88602931  4.8187586
+  7.24867005  6.52300688  5.17284736  2.03620244  5.2062381  10.13747086
+  5.80282456  4.24814692  5.76304321  3.92764098  4.45107535  4.0367042
+  3.1578201   5.02729212  7.2019339   4.52109849  4.58673327  3.75687966
+  5.72590874  3.43465513  4.85112642  4.03196464  5.86543118  3.27671261
+  3.48700378  5.99570684  5.16017257  7.02731262  4.56220263  6.92659455
+  4.97810735  5.84448072  5.6728509   6.17531094  5.91571008  2.27482113
+  3.59368272  4.2095767   4.40178482  5.37118768  3.62152793  6.15815838
+  4.87632355  4.70791917  2.28962452  3.55334207  4.52573046  5.4386965
+  3.74899339  5.17945746  3.96803089  3.30165097  5.42634199  4.60421712
+  7.14969051  5.79593331  6.74467254  4.35145509  9.11365885  3.86493637
+  1.93610307  5.27888584  4.46300427  3.08451558  6.54262567  7.18869045
+  3.08098041  4.946324    5.17448407  9.36946743  3.48039108  4.15821828
+  7.22221565  6.86633007  8.74261326 -0.10398885  6.65383107  9.67748237
+  4.00012193  5.33619618  4.30367543  1.43629808  6.68616782  5.69684464
+  5.00574792  3.43940833  7.20848608  8.42701971  2.21212613  3.91987173
+  2.29786147  6.86275737  3.81656653  3.71594197]
+
+Sample mean: 4.967364229032898
+  
+Sample standard deviation: 1.8406462382572457
+  
+95% confidence interval for the mean: (4.602140082264826, 5.33258837580097)
+'''
+we first generate some random data using the numpy.random.normal() function with mean 5, standard deviation 2, and size 100. 
+We then calculate the sample mean and standard deviation using the numpy.mean() and numpy.std() functions, respectively.
+We then set the desired confidence level to 0.95 and calculate the critical z-value using the scipy.stats.norm.ppf() function, which takes the probability as input and returns the critical z-value. 
+Since the sample size is large and the population standard deviation is known, we can use the formula Z = (x - mu) / (sigma / sqrt(n)) to calculate the standard normal distribution value (Z-score) corresponding to the sample mean.
+We then calculate the margin of error by multiplying the critical z-value by the standard error of the mean, which is the population standard deviation divided by the square root of the sample size. 
+Finally, we calculate the lower and upper bounds of the confidence interval by subtracting and adding the margin of error to the sample mean, respectively.
+'''
+
+# Here's an example of how to perform hypothesis testing in Python for the mean of a sample using a t-test:
+
+import numpy as np
+from scipy import stats
+
+# generate some random data
+data = np.random.normal(loc=5, scale=2, size=29)
+
+# set the null hypothesis value
+null_mean = 4.5
+
+# perform one-sample t-test
+t_stat, p_value = stats.ttest_1samp(data, null_mean)
+
+# print results
+print("Data:", data)
+print("Null hypothesis mean:", null_mean)
+print("Sample mean:", np.mean(data))
+print("T-statistic:", t_stat)
+print("P-value:", p_value)
+if p_value < 0.05:
+    print("Reject the null hypothesis")
+else:
+    print("Fail to reject the null hypothesis")
+    
+# Output
+Data: 
+[ 2.6976993   6.89891149  7.08529617  5.88375535  6.55807814  7.50557985
+  4.23477861  8.58967446  3.75953901  6.92383234  5.74641725  5.73479524
+  6.65107345  1.66592008  2.84603849  3.72154747  1.45529462  7.25689709
+  4.35850022  7.7848108   2.49709643  4.24206417  5.7595167  -0.22976098
+  4.77817066  6.07039455  2.3177079   6.93018335  5.05234045]
+
+Null hypothesis mean: 4.5
+  
+Sample mean: 4.992281125897192
+  
+t-statistic: 1.2056582294614182
+  
+P-value: 0.23803941013455565
+  
+Fail to reject the null hypothesis
+
+'''
+In this example, we first generate some random data using the numpy.random.normal() function with mean 5, standard deviation 2, and size 29. 
+We then set the null hypothesis value to 4.5.
+We perform a one-sample t-test using the scipy.stats.ttest_1samp() function, which takes the sample data and null hypothesis value as inputs and returns the t-statistic and p-value of the test.
+The t-statistic measures the difference between the sample mean and the null hypothesis value in terms of the standard error of the mean, while the p-value represents the probability of obtaining a t-statistic as extreme or more extreme than the observed one, assuming the null hypothesis is true.
+We print the results of the test, including the sample mean, t-statistic, and p-value. Finally, we use a significance level of 0.05 to determine whether to reject or fail to reject the null hypothesis. If the p-value is less than the significance level, we reject the null hypothesis, otherwise we fail to reject it.
+- Note that this example assumes that the sample data is normally distributed and that the population standard deviation is unknown and estimated from the sample. 
+- If these assumptions are not met, different tests may need to be used.
+'''
+
+
+
 
