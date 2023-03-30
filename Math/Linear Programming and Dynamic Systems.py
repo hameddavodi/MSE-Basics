5be1660fa47ad28bf8005a0953afcd020413953d
Hamed Davoodi <109058050+hameddavodi@users.noreply.github.com>
2023-03-30 17:17:47 +0200

    Update Linear Programming and Dynamic Systems.py

--- Math/Linear Programming and Dynamic Systems.py
+++ Math/Linear Programming and Dynamic Systems.py
@@ -123,6 +123,14 @@ t_span = [0, 5]
 # Solve the system of differential equations
 sol = solve_ivp(sys, t_span, z0)
 
+# Print the solution status
+print(sol)
+
+# Print the solution
+print('t\t x(t)\t y(t)')
+for t, x, y in zip(sol.t, sol.y[0], sol.y[1]):
+    print('{:.2f}\t {:.4f}\t {:.4f}'.format(t, x, y))
+
 # Plot the solution
 plt.plot(sol.t, sol.y[0], label='x(t)')
 plt.plot(sol.t, sol.y[1], label='y(t)')
@@ -133,3 +141,43 @@ plt.title('Solution to the System of Differential Equations')
 plt.show()
 
 # The output should be look like this: 
+# Status
+  message: The solver successfully reached the end of the integration interval.
+  success: True
+   status: 0
+        t: [ 0.000e+00  9.990e-04 ...  4.669e+00  5.000e+00]
+        y: [[ 1.000e+00  9.980e-01 ... -3.661e-06  1.286e-05]
+            [ 0.000e+00 -9.970e-04 ...  8.739e-05  4.324e-05]]
+      sol: None
+ t_events: None
+ y_events: None
+     nfev: 104
+     njev: 0
+      nlu: 0
+
+  # Points over the time 
+        
+        t	 x(t)	 y(t)
+0.00	 1.0000	 0.0000
+0.00	 0.9980	 -0.0010
+0.01	 0.9782	 -0.0107
+0.11	 0.7962	 -0.0887
+0.40	 0.4094	 -0.1752
+0.74	 0.1676	 -0.1533
+1.10	 0.0498	 -0.0984
+1.50	 0.0037	 -0.0500
+1.82	 -0.0065	 -0.0251
+2.15	 -0.0074	 -0.0114
+2.50	 -0.0054	 -0.0040
+2.89	 -0.0030	 -0.0008
+3.31	 -0.0013	 0.0002
+3.73	 -0.0005	 0.0003
+4.17	 -0.0001	 0.0002
+4.67	 -0.0000	 0.0001
+5.00	 0.0000	 0.0000
+
+  # Graph:
+  ...
+  
+
+
